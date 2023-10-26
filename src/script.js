3d0b1aefbe9019fb87565d2c6b0910e0d117b887
Matthew Begnoche <132032766+B3gnoche@users.noreply.github.com>
2023-10-26 09:23:04 -0500

    Project Complete
    Project is complete. Starting QA/QC and refactoring process.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,43 @@
 "use strict";
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  speed: 500,
+  loop: true,
+  autoHeight: false,
+  centeredSlides: true,
+  followFinger: true,
+  freeMode: false,
+  slideToClickedSlide: false,
+  slidesPerView: 1,
+  spaceBetween: "4%",
+  rewind: false,
+  mousewheel: {
+    forceToAxis: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  breakpoints: {
+    // mobile landscape
+    320: {
+      slidesPerView: 1.25,
+      spaceBetween: "4%",
+    },
+    // tablet
+    800: {
+      slidesPerView: 1.25,
+      spaceBetween: "4%",
+    },
+    // desktop
+    992: {
+      slidesPerView: 2.25,
+      spaceBetween: "2%",
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
